b804388a4d7f9b5e2c27a38aac7d8ab2fe46621c
Meredith Grimes <meredithagrimes@gmail.com>
2020-03-20 13:52:58 -0400

    fixed copy to clipboard function, set requirements to select 8 to 128 characters, improved user experience to present user with prompt when generate password is clicked

--- script.js
+++ script.js
@@ -7,72 +7,78 @@ var oneOfEachCharacters = [];
 var finalPassword = [];
 var newArray = [];
 
-var lengthChoice = parseInt(prompt("How many characters do you want in the password?"));
-var uppercaseChoice = confirm("Do you want uppercase letters in the password?");
-var lowercaseChoice = confirm("Do you want lowercase letters in the password?");
-var numberChoice = confirm("Do you want numbers in the password?");
-var specialcharactersChoice = confirm("Do you want special characters in the password?");
-
-if (uppercaseChoice) {
-    selectedCharacters += uppercase;
-    oneOfEachCharacters.push(uppercase.charAt(Math.floor(Math.random() * (uppercase.length))));
-    console.log(selectedCharacters);
-    console.log(oneOfEachCharacters);
-}
-
-if (lowercaseChoice) {
-    selectedCharacters += lowercase;
-    oneOfEachCharacters.push(lowercase.charAt(Math.floor(Math.random() * (lowercase.length))));
-    console.log(selectedCharacters);
-    console.log(oneOfEachCharacters);
-}
-
-if (numberChoice) {
-    selectedCharacters += numbers;
-    oneOfEachCharacters.push(numbers.charAt(Math.floor(Math.random() * (numbers.length))));
-    console.log(selectedCharacters);
-    console.log(oneOfEachCharacters);
-}
-
-if (specialcharactersChoice) {
-    selectedCharacters += characters;
-    oneOfEachCharacters.push(characters.charAt(Math.floor(Math.random() * (characters.length))));
-    console.log(selectedCharacters);
-    console.log(oneOfEachCharacters);
-}
-
-if (lengthChoice >= 8 && lengthChoice <= 128) {
-    for (i = +0; i < lengthChoice; i++) {
-        finalPassword.push(selectedCharacters[Math.floor(Math.random() * (selectedCharacters.length))])
-    }
-    console.log(finalPassword)
-
-    // for (z=0; 1; z++) {
-    //     finalPassword[z] = oneOfEachCharacters[z];
-    // }
-
-    // console.log(finalPassword);
-} else {
-
-
-
-// Generate password in textbox
 function generate() {
-    if (lengthChoice >= 8 && lengthChoice <= 128) {
-        for (i <= 1; i < lengthChoice; i++) {
+
+    var lengthChoice = parseInt(prompt("How many characters do you want in the password?"));
+
+    // This is where the password length is required to be min 8 and max 128 characters.
+    if (lengthChoice > 7 && lengthChoice < 129) {
+        alert("Please confirm you want " + lengthChoice + " characters in your password.");
+    } else if (Number.isNaN(lengthChoice)) {
+        alert("That is not an option.");
+        password();
+    } else {
+        alert("You have not entered a valid password length. You must choose between 8 and 128 characters.");
+        password();
+    }
+
+    var uppercaseChoice = confirm("Do you want uppercase letters in the password?");
+
+    if (uppercaseChoice) {
+        selectedCharacters += uppercase;
+        oneOfEachCharacters.push(uppercase.charAt(Math.floor(Math.random() * (uppercase.length))));
+        console.log(selectedCharacters);
+        console.log(oneOfEachCharacters);
+    }
+
+    var lowercaseChoice = confirm("Do you want lowercase letters in the password?");
+
+    if (lowercaseChoice) {
+        selectedCharacters += lowercase;
+        oneOfEachCharacters.push(lowercase.charAt(Math.floor(Math.random() * (lowercase.length))));
+        console.log(selectedCharacters);
+        console.log(oneOfEachCharacters);
+    }
+
+    var numberChoice = confirm("Do you want numbers in the password?");
+
+    if (numberChoice) {
+        selectedCharacters += numbers;
+        oneOfEachCharacters.push(numbers.charAt(Math.floor(Math.random() * (numbers.length))));
+        console.log(selectedCharacters);
+        console.log(oneOfEachCharacters);
+    }
+
+    var specialcharactersChoice = confirm("Do you want special characters in the password?");
+
+    if (specialcharactersChoice) {
+        selectedCharacters += characters;
+        oneOfEachCharacters.push(characters.charAt(Math.floor(Math.random() * (characters.length))));
+        console.log(selectedCharacters);
+        console.log(oneOfEachCharacters);
+    }
+
+    if (lengthChoice > 7 && lengthChoice < 129) {
+        for (i = +0; i < lengthChoice; i++) {
             finalPassword.push(selectedCharacters[Math.floor(Math.random() * (selectedCharacters.length))])
         }
-        document.getElementById("passwordBox").innerHTML = answer;
+        console.log(finalPassword)
+
+        // This displays the generated password in the text area
+        if (lengthChoice > 7 && lengthChoice < 129) {
+            for (i <= 1; i < lengthChoice; i++) {
+                finalPassword.push(selectedCharacters[Math.floor(Math.random() * (selectedCharacters.length))])
+            }
+            document.getElementById("passwordBox").textContent = finalPassword;
+        }
+    }
 }
 
 // Copy to clipboard function
-
-function clipboard() {
+function copyPassword() {
     var copyText = document.getElementById("passwordBox");
     copyText.select();
     copyText.setSelectionRange(0, 99999);
     document.execCommand("Copy");
-    alert("Copied the Text" + finalPassword);
-  }   
-  
-}}
+    alert("Copied the Text: " + finalPassword);
+}
